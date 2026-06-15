e94202320e8e3e5ef8193aaedb5363f920a29d0d
sunnivanordbjerga <sunnivanordbjerga@gmail.com>
2026-06-15 18:12:32 +0200

    style: ruff format

--- tests/seed_data/test_bike_generator.py
+++ tests/seed_data/test_bike_generator.py
@@ -1,7 +1,7 @@
 from seed_data.generators.bike_generator import generate_bikes
 
-STATION_IDS = [1,2,3,4]
-ACTIVITY_STATUS_IDS = [5,6,7,8]
+STATION_IDS = [1, 2, 3, 4]
+ACTIVITY_STATUS_IDS = [5, 6, 7, 8]
 
 
 def test_generate_bikes_returns_correct_amount():
