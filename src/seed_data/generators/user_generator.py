9448bdacc6a9f90b5a863d0fac2e05ea0bb8989d
sunnivanordbjerga <sunnivanordbjerga@gmail.com>
2026-06-15 14:09:28 +0200

    refactor(generators): added international users

--- src/seed_data/generators/user_generator.py
+++ src/seed_data/generators/user_generator.py
@@ -1,16 +1,21 @@
 """Generates fake users."""
 
-from random import uniform
+from random import uniform, random
 from dataclasses import dataclass
-from faker import Faker
 
-fake = Faker("no_NO")
+from faker import Faker
 
 BERGEN_MIN_LAT = 60.1017
 BERGEN_MAX_LAT = 60.6167
 
 BERGEN_MIN_LON = 5.0833
 BERGEN_MAX_LON = 5.8533
+
+INTERNATIONAL_USER_PERCENTAGE = 0.15
+INTERNATIONAL_LOCALES = ["en_US", "de_DE", "pl_PL", "uk_UA"]
+
+fake_no = Faker("no_NO")
+fake_inter = Faker(INTERNATIONAL_LOCALES)
 
 
 @dataclass
@@ -32,14 +37,19 @@ def generate_users(num_users: int) -> list[GeneratedUser]:
     Returns:
         A list of generated users.
     """
+    users = []
 
-    return [
-        GeneratedUser(
-            first_name=fake.first_name(),
-            last_name=fake.last_name(),
-            phone_number=fake.phone_number(),
-            latitude=round(uniform(BERGEN_MIN_LAT, BERGEN_MAX_LAT), 6),
-            longitude=round(uniform(BERGEN_MIN_LON, BERGEN_MAX_LON), 6),
+    for _ in range(num_users):
+        fake = fake_no if random() > INTERNATIONAL_USER_PERCENTAGE else fake_inter
+
+        users.append(
+            GeneratedUser(
+                first_name=fake.first_name(),
+                last_name=fake.last_name(),
+                phone_number=fake_no.phone_number(),
+                latitude=round(uniform(BERGEN_MIN_LAT, BERGEN_MAX_LAT), 6),
+                longitude=round(uniform(BERGEN_MIN_LON, BERGEN_MAX_LON), 6),
+            )
         )
-        for _ in range(num_users)
-    ]
+
+    return users
