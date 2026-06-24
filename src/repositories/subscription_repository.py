4e301cad1c4de9d73463fa5edc124efbeb88076d
Sunniva <sunnivanordbjerga@gmail.com>
2026-06-25 01:56:31 +0200

    feat(repositories): add revenue and subscription count methods

--- src/repositories/subscription_repository.py
+++ src/repositories/subscription_repository.py
@@ -5,7 +5,7 @@ from models.subscription import Subscription
 from models.subscription_type import SubscriptionType
 
 
-def get_subscriptions():
+def get_subscriptions() -> list[Subscription]:
     with get_connection() as conn:
         subscriptions = conn.execute("""
                                      SELECT S.SubscriptionId,
@@ -29,7 +29,7 @@ def get_subscriptions():
     ]
 
 
-def get_subscription_types():
+def get_subscription_types() -> list[SubscriptionType]:
     with get_connection() as conn:
         types = conn.execute("""
         SELECT SubscriptionTypeId, Description, DurationInDays, Price
@@ -45,3 +45,71 @@ def get_subscription_types():
         )
         for row in types
     ]
+
+
+def get_subscription_count_by_month() -> dict[str, int]:
+    """Return an overview of subscriptions sold per month over the last year."""
+    with get_connection() as conn:
+        return {
+            month: sub_count
+            for month, sub_count in conn.execute("""
+                                                 SELECT strftime('%Y-%m', StartDate) as YearAndMonth,
+                                                        COUNT(SubscriptionID)        AS SubscriptionCount
+                                                 FROM Subscription
+                                                 WHERE StartDate >= date('now', '-1 year')
+                                                 GROUP BY YearAndMonth
+                                                 ORDER BY YearAndMonth;
+                                                 """).fetchall()
+        }
+
+
+def get_subscription_count_by_type() -> dict[str, int]:
+    """Return an overview of subscriptions sold per subscription type over the last year."""
+    with get_connection() as conn:
+        return {
+            sub_type: sub_count
+            for sub_type, sub_count in conn.execute("""
+                                                    SELECT ST.Description,
+                                                           COUNT(S.SubscriptionTypeID) AS SubscriptionCount
+                                                    FROM Subscription AS S
+                                                             JOIN SubscriptionType AS ST ON S.SubscriptionTypeID = ST.SubscriptionTypeID
+                                                    WHERE S.StartDate >= date('now', '-1 year')
+                                                    GROUP BY ST.SubscriptionTypeID, ST.Description, ST.DurationInDays
+                                                    ORDER BY ST.DurationInDays;
+                                                    """).fetchall()
+        }
+
+
+def get_revenue_by_month() -> dict[str, int]:
+    """Return an overview of revenue per month over the last year."""
+    with get_connection() as conn:
+        return {
+            month: revenue
+            for month, revenue in conn.execute("""
+                                               SELECT strftime('%Y-%m', S.StartDate) as YearAndMonth,
+                                                      SUM(ST.Price)                  AS Revenue
+                                               FROM Subscription AS S
+                                                        JOIN SubscriptionType AS ST ON S.SubscriptionTypeID = ST.SubscriptionTypeID
+                                               WHERE S.StartDate >= date('now', '-1 year')
+                                               GROUP BY YearAndMonth
+                                               ORDER BY YearAndMonth;
+                                               """).fetchall()
+        }
+
+
+def get_revenue_by_subscription_type() -> dict[str, int]:
+    """Return an overview of revenue per subscription type over the last year."""
+    with get_connection() as conn:
+        return {
+            sub_type: revenue
+            for sub_type, revenue in conn.execute("""
+                                                  SELECT ST.Description,
+                                                         SUM(ST.Price) AS Revenue
+                                                  FROM Subscription AS S
+                                                           JOIN SubscriptionType AS ST ON S.SubscriptionTypeID = ST.SubscriptionTypeID
+                                                  WHERE S.StartDate >= date('now', '-1 year')
+                                                  GROUP BY ST.SubscriptionTypeID, ST.Description, ST.DurationInDays
+                                                  ORDER BY ST.DurationInDays;
+                                                  """).fetchall()
+        }
+
